bc80aa7cb5e03316163718d4ba3540c54b5382cd
darshanthorat99 <113414840+darshanthorat99@users.noreply.github.com>
2022-09-22 10:19:37 +0530

    commiy

--- loop/ass5.cs
+++ loop/ass5.cs
@@ -8,12 +8,20 @@ namespace loop
 {
     internal class ass5
     {
-    //    static void Main(string[] args)
-    //    {
-    //        Console.WriteLine("enter two number");
-    //        int n1 = Convert.ToInt32(Console.ReadLine());
-    //        int n2 = Convert.ToInt32(Console.ReadLine());
-
-      //  }
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("enter two number");
+        //    int n1=Convert.ToInt32(Console.ReadLine());
+        //    int n2=Convert.ToInt32(Console.ReadLine());
+        //    for (int i = 2; i < 9; ++i)
+        //    {
+        //        int n3 = n1 + n2;
+        //        Console.WriteLine($"{n3}");
+        //        n1 = n2;
+        //        n2 = n3;
+        //    }
+        
+        
+        //}
     }
 }
